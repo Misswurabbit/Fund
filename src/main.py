dc1da47c299ae8420763d78d2e8e2ac45d5ba661
Misswurabbit <37863272+Misswurabbit@users.noreply.github.com>
2018-11-10 15:03:11 +0800

    for total data

--- src/main.py
+++ src/main.py
@@ -1,16 +1,56 @@
 import pandas as pd
 from os import path
 from config import Config
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
-original_nav_csv = path.abspath(path.join(__file__, "../../../data/original/ODS_MDS.BSHEET.csv"))
-file = pd.read_csv(original_nav_csv, encoding="gb18030")
-temp = file.loc[:, ["SYMBOL", "BSHEET10", "BSHEET11", "BSHEET12", "BSHEET13"]]
-temp.dropna(inplace=True)
-temp.reset_index(drop=True,inplace=True)
-nameusing = pd.DataFrame(data={"SYMBOL":Config.name_list})
-namelist = Config.name_list
-absense = namelist
-for name in temp["SYMBOL"]:
-    if int(name) in namelist:
-        absense.remove(name)
+# original_bsheet_csv = path.abspath(path.join(__file__, "../../data/original/ODS_MDS.BSHEET.csv"))
+# file = pd.read_csv(original_bsheet_csv, encoding="gb18030")
+# add_data = file.loc[:, ["SYMBOL", "BSHEET10", "BSHEET11", "BSHEET55", "BSHEET56"]]
+# add_data.reset_index(drop=True, inplace=True)
+# add_data["SYMBOL"] = pd.to_numeric(add_data["SYMBOL"], errors="coerce")
+# add_data.dropna(inplace=True)
+# labelname = ["p5", "p10", "p15", "p-5", "p-10", "p-15"]
+# combine = []
+# for name in [1]:
+#     data = pd.read_csv("../data/processed_data/" + str(name) + "__processed_data.csv")
+#     data = data[(data["train"] >= 45) & (data["test"] >= 15)]
+#     data["SYMBOL"] = [name for num in range(len(data))]
+#     combine.append(data)
+#     # add_data = temp[temp["SYMBOL"] == name]
+# total = combine[0]
+# # for num in range(1, len(combine)):
+# #     total = total.append(combine[num])
+# # total.to_csv("total.csv")
+# total = total.merge(add_data, on="SYMBOL", how="outer")
+# total.dropna(inplace=True)
+# total.reset_index(drop=True,inplace=True)
+# #total.drop()
+# # total.to_csv("__total__.csv", index=False)
+total = pd.read_csv("__total__.csv")
+index = ["BSHEET10", "BSHEET11", "BSHEET55", "BSHEET56"]
+for num in  range(89):
+    index.append(str(num))
+train = total.loc[:, index]
+target = total.loc[:, labelname].astype(bool)
+x_train, x_test, y_train, y_test = train_test_split(train, target, test_size=0.3)
+model = RandomForestClassifier()
+model.fit(x_train,y_train)
+result = model.predict(x_test)
+have = accuracy_score(y_test,result)
+print("have:   "+str(have))
+
+model = RandomForestClassifier()
+x_train.drop(["BSHEET10", "BSHEET11", "BSHEET55", "BSHEET56"],inplace=True,axis=1)
+x_test.drop(["BSHEET10", "BSHEET11", "BSHEET55", "BSHEET56"],inplace=True,axis=1)
+model.fit(x_train,y_train)
+result = model.predict(x_test)
+nohave = accuracy_score(y_test,result)
+print("nohave:    "+str(nohave))
+# namelist = Config.name_list
+# absense = namelist
+# for name in temp["SYMBOL"]:
+#     if int(name) in namelist:
+#         absense.remove(name)
 print("Over")
--- src/model_training_prediction.py
+++ src/model_training_prediction.py
@@ -1,4 +1,4 @@
-from Project.src.config import Config
+from src.config import Config
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
@@ -7,40 +7,100 @@ from sklearn.metrics import accuracy_score
 
 def model_training():
     name_list = Config.name_list
+    score_dict = {"p5": [], "p10": [], "p15": [], "p-5": [], "p-10": [], "p-15": [], "num": []}
+    with open("accuracy.txt", "w") as f:
+        f.seek(0)
+        f.truncate()
     for name in name_list:
         data = pd.read_csv("../data/processed_data/" + str(name) + "__processed_data.csv")
-        train_date_index = [num for num in range(1, 90)]
-        train = data.iloc[:, train_date_index]
-        # target = data.iloc[:,91]
-        target = data.iloc[:, [num for num in range(91, data.shape[1])]]
-        score = 0
+        data = data[(data["train"] >= 45) & (data["test"] >= 15)]
+        train = data.iloc[:, 1:90]
+        target = data.iloc[:, -6:]
+        score_dict["num"].append(len(data))
         for num in range(6):
             labelname = ["p5", "p10", "p15", "p-5", "p-10", "p-15"]
             x_train, x_test, y_train, y_test = train_test_split(train, target.iloc[:, num], test_size=0.3)
+            for index in range(4):
+                temp = [x_train, x_test, y_train, y_test]
+                temp_name = ["x_train", "x_test", "y_train", "y_test"]
+                temp[index].to_csv(
+                    "../data/temp/" + str(name) + "__" + str(labelname[num]) + "__" + temp_name[index] + "__data.csv",
+                    index=False)
+            # x_train.to_csv("../data/temp/" + str(name) + "__" + str(labelname[num]) + "__" + "_x-train_data.csv",
+            #                index=False)
+            # x_test.to_csv("../data/temp/" + str(name) + "__" + str(labelname[num]) + "__" + "_x-test_data.csv",
+            #               index=False)
+            # y_train.to_csv("../data/temp/" + str(name) + "__" + str(labelname[num]) + "__" + "_y-train_data.csv",
+            #                index=False)
+            # y_test.to_csv("../data/temp/" + str(name) + "__" + str(labelname[num]) + "__" + "_y-test_data.csv",
+            #               index=False)
             clf = RandomForestClassifier()
             clf.fit(x_train, y_train)
             result = clf.predict(x_test)
             pre_result = clf.predict_proba(x_test)
-            if pre_result.shape[1]==2:
-                out_put = pd.DataFrame(
-                    data={labelname[num]: y_test, "predict": result, "predict_proba_False": pre_result[:, 0],
-                          "predict_proba_True": pre_result[:, 1]})
+            score = accuracy_score(y_test, result)
+            score_dict[labelname[num]].append(score)
+            if pre_result.shape[1] == 2:
+                proba = [[temp[0], temp[1]] for temp in pre_result]
             else:
                 if result[0]:
-                    out_put = pd.DataFrame(
-                        data={labelname[num]: y_test, "predict": result, "predict_proba_False": [0 for num in range(pre_result.shape[0])],
-                              "predict_proba_True": pre_result[:, 0]})
+                    proba = [[0, 1] for temp in range(len(pre_result))]
                 else:
-                    out_put = pd.DataFrame(
-                        data={labelname[num]: y_test, "predict": result, "predict_proba_False": pre_result[:, 0],
-                              "predict_proba_True": [0 for num in range(pre_result.shape[0])]})
-
-            out_put.to_csv("../data/result/" + str(name) + "__" + str(labelname[num]) + "__" + "_processed_data.csv")
+                    proba = [[1, 0] for temp in range(len(pre_result))]
+            out_put = pd.DataFrame({"result": result, "proba": proba})
+            # if pre_result.shape[1] == 2:
+            #     out_put = pd.DataFrame(
+            #         data={labelname[num]: y_test, "predict": result, "predict_proba_False": pre_result[:, 0],
+            #               "predict_proba_True": pre_result[:, 1]})
+            # else:
+            #     if result[0]:
+            #         out_put = pd.DataFrame(
+            #             data={labelname[num]: y_test, "predict": result,
+            #                   "predict_proba_False": [0 for num in range(pre_result.shape[0])],
+            #                   "predict_proba_True": pre_result[:, 0]})
+            #     else:
+            #         out_put = pd.DataFrame(
+            #             data={labelname[num]: y_test, "predict": result, "predict_proba_False": pre_result[:, 0],
+            #                   "predict_proba_True": [0 for num in range(pre_result.shape[0])]})
+            out_put.to_csv("../data/result/" + str(name) + "__" + str(labelname[num]) + "__" + "_processed_data.csv",
+                           index=False)
             with open("accuracy.txt", "a+") as f:
-                f.write(str(name) + " fund " + labelname[num] + " RF_accuracy:" + str(
-                    accuracy_score(y_test, result)) + " \n")
-        #print(score / 6)
+                f.write(str(name) + " fund " + labelname[num] + " RF_accuracy:" + str(score) + " \n")
         print(name)
+    pd.DataFrame(data=score_dict).to_csv("score_dict.csv")
 
 
-model_training()
+#model_training()
+
+
+
+# labelname = ["p5", "p10", "p15", "p-5", "p-10", "p-15"]
+# for label in labelname:
+#     for type in ["y_train", "y_test"]:
+#         temp = []
+#         for name in Config.name_list:
+#             data = pd.read_csv("../data/temp/" + str(name) + "__" + str(label) + "__" + type + "__data.csv",names=["target"])
+#             temp.append(data)
+#         result = temp[0]
+#         for num in range(1, len(temp)):
+#             result = result.append(temp[num])
+#         result.to_csv("__" + str(label) + "__" + type + "__data.csv", index=False)
+#         print("__" + str(label) + "__" + type + "__data.csv   is  Over")
+
+labelname = ["p5", "p10", "p15", "p-5", "p-10", "p-15"]
+score_dic = {"p5": [], "p10": [], "p15": [], "p-5": [], "p-10": [], "p-15": []}
+for label in labelname:
+    x_train = pd.read_csv("__" + str(label) + "__" + "x_train" + "__data.csv")
+    y_train = pd.read_csv("__" + str(label) + "__" + "y_train" + "__data.csv")
+    x_test = pd.read_csv("__" + str(label) + "__" + "x_test" + "__data.csv")
+    y_test = pd.read_csv("__" + str(label) + "__" + "y_test" + "__data.csv")
+    model = RandomForestClassifier()
+    model.fit(x_train,y_train)
+    result = model.predict(x_test)
+    #score = accuracy_score(y_test,result)
+    score_dic[label].append(accuracy_score(y_test,result))
+#pd.DataFrame().to_csv("all__score.csv", index=False)
+output = pd.DataFrame(score_dic)
+output.to_csv("all_acore.csv")
+#print("__" + str(label) + "__" + type + "__data.csv   is  Over")
+
